5cfe4eecef478f3da3937228a83d8382619cb60d
0xBA5E64 <26796481+0xBA5E64@users.noreply.github.com>
2026-07-13 00:06:39 +0200

    refactor: General index.vue cleanup and refactoring

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-
-let imageWyrmLogo: number[][] = [
+// prettier-ignore
+let wyrm_graphic_matrix: number[][] = [
   [0,0,0,1,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,0,0,0,0],
   [0,0,0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,0],
   [0,0,0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,1,1,0,1,1,0,0,0],
@@ -11,9 +11,9 @@ let imageWyrmLogo: number[][] = [
   [0,0,0,1,1,0,1,1,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0,0,0],
   [0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0,0,0],
   [0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,0,0,0,0,0,0,1,0,0,0]
-]
-
-let imageDrgnLogo: number[][] = [
+];
+// prettier-ignore
+let drgn_graphic_matrix: number[][] = [
   [0,0,0,0,1,1,1,1,1,0,0,0,1,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0],
   [0,0,0,0,1,1,1,1,1,1,0,0,1,1,1,1,1,1,0,0,1,1,1,1,1,1,0,0,1,1,1,0,0,0,1,1,0,0,0,0],
   [0,0,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,0,0,0,0,1,1,1,1,0,0,1,1,0,0,0,0],
@@ -24,42 +24,35 @@ let imageDrgnLogo: number[][] = [
   [0,0,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,1,1,0,0,0,0],
   [0,0,0,0,1,1,1,1,1,1,0,0,1,1,0,0,1,1,0,0,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,1,0,0,0,0],
   [0,0,0,0,1,1,1,1,1,0,0,0,1,1,0,0,1,1,0,0,0,1,1,1,1,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0]
-]
-
-// document.getElementById("linkBox").children[2].addEventListener("mouseenter", function(){renderImage(imageDrgnLogo)})
+];
 </script>
 
 <template>
-
     <section id="greeter">
-
         <div class="center-container">
-            <HeroGraphic :graphic=imageWyrmLogo />
+            <HeroGraphic :graphic="wyrm_graphic_matrix" />
             <SocialLinks />
         </div>
-
-        <!-- <CryptoAddresses /> -->
     </section>
 
     <section id="information">
-
-        <div class="aboutBar">
-            <p class="title"
-                >Bio<span style="opacity: 0.5">graphy</span></p
-            >
-            <p class="disclaimer"
-                >Based off of Midnight Scrivener's
+        <div class="section-label">
+            <p class="title">
+                <Icon name="ri:arrow-down-s-line" size="32" />Bio<span
+                    >graphy</span
+                >
+            </p>
+            <p class="disclaimer">
+                Based off of Midnight Scrivener's
                 <a
                     href="https://midnight-scrivener.tumblr.com/post/169992996839/what-if-people-we-consider-collectors-today-were"
                     >flash fiction</a
-                ></p
-            >
+                >
+            </p>
         </div>
 
         <BiographyDialogue />
-
     </section>
-
 </template>
 
 <style>
@@ -70,8 +63,6 @@ section {
     padding: 0;
     margin: 0;
 }
-
-/* TOP SECTION: "GREETER" */
 
 section#greeter {
     height: 100%;
@@ -85,60 +76,35 @@ section#greeter {
     display: inline-grid;
 }
 
-/* SECTION: "INFORMATION" */
-
-section#nformation {
-    position: relative;
-    /* Potential section-specific adjustments here ;) */
-}
-
-section#information .aboutBar {
+section .section-label {
     position: absolute;
-    top: -90px;
+    bottom: 100%;
     left: 50%;
     transform: translate(-50%);
     width: 80%;
     max-width: 2000px;
     height: 90px;
-    opacity: 0.5;
     display: flex;
     justify-content: space-between;
     align-items: center;
 }
 
-section#information .aboutBar .title {
+section .section-label .title {
     display: flex;
     align-items: center;
 }
 
-section#information .aboutBar .title:before {
-    position: relative;
-    display: inline-block;
-    content: "";
-    width: 0.65em;
-    height: 0.65em;
-    border-right: solid white 0.15em;
-    border-bottom: solid white 0.15em;
-    margin: 0em 0.8em;
-    /*background: linear-gradient(
-    45deg,
-    transparent   0%,
-    transparent  45%,
-    white      45%,
-    white      55%,
-    transparent  55%,
-    transparent 100%
-  );*/
-    transform: translateY(-25%) rotate(45deg);
+section .section-label .title span {
+    opacity: 0.5;
 }
 
-section#information .aboutBar .disclaimer {
+section .section-label .disclaimer {
+    margin-left: 50px;
     text-align: right;
-    width: 65%;
 }
 
 @media only screen and (orientation: portrait) {
-    section#information .aboutBar .disclaimer {
+    section .section-label .disclaimer {
         font-size: 0.75em;
     }
 }
