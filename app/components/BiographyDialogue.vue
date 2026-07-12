33c5b89e3add6f78650a91c17ab914dc29e4c694
0xBA5E64 <26796481+0xBA5E64@users.noreply.github.com>
2026-07-12 18:53:42 +0200

    fix: Update Biography to reflect current site body

--- app/components/BiographyDialogue.vue
+++ app/components/BiographyDialogue.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="biography">
         <p class="wyrm">
-            I am the dragon that calls themselves <b>Dovah</b>, and
+            I am the dragon who calls themselves <b>Dovah</b>, and
             <a href="https://file.wyrm.host/">this</a>, is my hoard.
         </p>
         <p class="othr">You... don't look really look like a dragon?</p>
@@ -9,7 +9,7 @@
             Well, hardly anyone does these days. As times have changed so have
             we. The scales and wings worked along the dinosaurs. But we quickly
             learned that wasn't going to fly with you people. Though I do
-            admittedly miss our kin's tails.
+            admittedly miss our tails.
         </p>
         <p class="othr">You were around all the way back to the dinosaurs?</p>
         <p class="wyrm">
@@ -43,7 +43,7 @@
         <p class="othr">Forgive me but, that seems unsafe to slumber on</p>
         <p class="wyrm">
             Military-grade casing is a small price to pay for a safe
-            <i>and</i> self-heating hoard in winter.
+            <i>and</i> self-heating hoard in the winter.
         </p>
         <p class="othr">
             Wow. So all these things... really mean a lot to you, huh?
@@ -52,6 +52,36 @@
             They are my memories. All the things that have helped shape me into
             the being who stands here talking to you now. Every last one of
             them, in some way, is a fragment of my very self.
+        </p>
+        <p class="othr">
+            I see... So, you'd probably be greatly offended by me asking to lend
+            your copy of Monotone Blue then, I'm guessing?
+        </p>
+        <p class="wyrm">
+            That would perhaps be a safe bet with others of my kin, however, not
+            with me. The very nature of digital media is very much different
+            from that of the physical, as is its relation to the concept of
+            scarcity. In physical space, handing you a book could mean I'd never
+            see it return. But, in digital space, to hand someone something is
+            natively an act of copying. We either conciously do this from act of
+            a user's copy-and-paste, or from the less apparent technical
+            processes computers loading data from storage to memory; all the
+            same, data is replicated, and none is given up. To limit this is to
+            deny the very nature of digital information, a foolish notion which
+            shall always falter, one way or another. The only true scarcity to
+            worry of is that of loss time, something naturally remidied by
+            simply sharing with one another, and each maintaining our own
+            "hoards" of sorts.
+        </p>
+        <p class="wyrm">
+            Dragons hoard the things they do because they matter to them,
+            because they bring them joy, comfort and happiness. Dragons are
+            protective of their hoards, because they, like people, don't want to
+            lose what matters to them the most. I am much the same; driven to
+            protect of what matters to me. Yet, things "taken" from my hoard,
+            are not taken away from me. For me, every file "taken", every copy
+            made, simply assures me further, that what matters for me won't be
+            lost again.
         </p>
     </div>
 </template>
