5597b3e608a4c09b9096c1a7ed10aa1928fe155e
0xBA5E64 <26796481+0xBA5E64@users.noreply.github.com>
2026-07-14 21:55:35 +0200

    refactor: Update biography text and styling thereof

--- app/components/BiographyDialogue.vue
+++ app/components/BiographyDialogue.vue
@@ -4,23 +4,27 @@
             I am the dragon who calls themselves <b>Dovah</b>, and
             <a href="https://file.wyrm.host/">this</a>, is my hoard.
         </p>
-        <p class="othr">You... don't look really look like a dragon?</p>
+        <p class="othr">
+            Forgive me but, you... don't look really look like a dragon?
+        </p>
         <p class="wyrm">
             Well, hardly anyone does these days. As times have changed so have
-            we. The scales and wings worked along the dinosaurs. But we quickly
-            learned that wasn't going to fly with you people. Though I do
-            admittedly miss our tails.
+            we. The scales and wings may have worked though yester-ages, but we
+            quickly learned that wasn't going to fly with you people. Though I
+            do admittedly miss the tails.
         </p>
-        <p class="othr">You were around all the way back to the dinosaurs?</p>
+        <p class="othr">
+            Wait, you were around all the way back to the dinosaurs?
+        </p>
         <p class="wyrm">
             Well, not <i>me</i>, personally. How old do you think I am?
         </p>
         <p class="othr">...There is no safe answer to that, is there?</p>
-        <p class="wyrm">No.</p>
-        <p class="othr">So... when you say this is your hoard...?</p>
+        <p class="wyrm">Smart catch.</p>
+        <p class="othr">So... when you say this is your hoard-?</p>
         <p class="wyrm">
             All dragons have one. Some stick to the old gold and jewels thing,
-            but that is somewhat cliché these days. Most of us nowadays like our
+            but that is somewhat kitschy these days. Most of us now prefer our
             hoards to be a little bit more sophisticated than 'shiny.'
         </p>
         <p class="othr">Like what?</p>
@@ -29,58 +33,65 @@
             the year over dozens of centuries. Dragons that collect the petals
             of flowers left on the graves of loved ones. Dragons that keep and
             care for soft toys and comfort items, left behind by children as
-            they've grown up. Dragons that guard happy memories and shards of
+            they grew up. Dragons that guard happy memories and shards of
             sunlight, kept safe for rainy days.
         </p>
-        <p class="wyrm">
-            I myself have found my calling in by nature of the digital age; 1s &
-            0s. These two primitives let me freely build my hoard out of most
-            anything, at unprecidented density. Music, art, film and writings,
-            and not just the famed works of noted names, but just so those of
-            lesser praise. I catch all that which catches my eye in the digital
-            stream, and I do my best to uphold my sanctuary of information.
+        <p class="othr">
+            ...and for you? Computers? Licorice? Tangled balls of cable?
         </p>
-        <p class="othr">Forgive me but, that seems unsafe to slumber on</p>
+        <p class="wyrm">
+            I myself found my calling in nature of the digital age; ones &
+            zeroes, carefully arranged like embroidery. These two primitives let
+            me fill my hoard with records of most anything, at an unprecidented
+            density. Music, art, film and writing, not just the famed works of
+            noted names, but just so those of lesser praise. I catch all that
+            which catches my eye in the digital stream, and I do my best to
+            uphold my sanctuary of information.
+        </p>
+        <p class="othr">
+            Forgive me but, don't your kind also nest on their hoards? That
+            seems unsafe to slumber on.
+        </p>
         <p class="wyrm">
             Military-grade casing is a small price to pay for a safe
-            <i>and</i> self-heating hoard in the winter.
+            <i>and</i> self-heating hoard for the winter.
         </p>
         <p class="othr">
-            Wow. So all these things... really mean a lot to you, huh?
+            Wow. So, all these things... really mean a lot to you, huh?
         </p>
         <p class="wyrm">
-            They are my memories. All the things that have helped shape me into
-            the being who stands here talking to you now. Every last one of
-            them, in some way, is a fragment of my very self.
+            They are my memories. Fragments of myself, like scales, that have
+            shaped me into whom stands here talking to you now. Every last one
+            of these things, in some way, is a fragment of my very self.
         </p>
         <p class="othr">
-            I see... So, you'd probably be greatly offended by me asking to lend
-            your copy of Monotone Blue then, I'm guessing?
+            I see... So, I'm guessing you'd probably be greatly offended by me
+            asking to lend your copy of Dorohedoro?
         </p>
         <p class="wyrm">
-            That would perhaps be a safe bet with others of my kin, however, not
-            with me. The very nature of digital media is very much different
-            from that of the physical, as is its relation to the concept of
-            scarcity. In physical space, handing you a book could mean I'd never
-            see it return. But, in digital space, to hand someone something is
-            natively an act of copying. We either conciously do this from act of
-            a user's copy-and-paste, or from the less apparent technical
-            processes computers loading data from storage to memory; all the
-            same, data is replicated, and none is given up. To limit this is to
-            deny the very nature of digital information, a foolish notion which
-            shall always falter, one way or another. The only true scarcity to
-            worry of is that of loss time, something naturally remidied by
-            simply sharing with one another, and each maintaining our own
-            "hoards" of sorts.
+            That would historically be a risk to ask of my kin, however, not for
+            me. The very nature of the digital is so very different from that of
+            the physical, now decoupled from any non-fungible concept of
+            scarcity. In physical space, handing you a book would mean losing it
+            for a while, or worse yet, forever. But, here in the digital age, to
+            share is no longer finite. We may be more or less concious of this
+            act of duplicaton, from an intended copy-and-paste, to the less
+            apparent technical processes by which all computers transparently
+            load data from storage to memory; all the same, data is replicated,
+            and none is lost. To limit this is to deny the very nature of
+            digital information, a foolish notion which shall always falter one
+            way or another. The only true scarcity to fear is that of loss to
+            medium failure, something naturally remidied by simply sharing with
+            one another, and each maintaining our own hoards.
         </p>
         <p class="wyrm">
             Dragons hoard the things they do because they matter to them,
-            because they bring them joy, comfort and happiness. Dragons are
-            protective of their hoards, because they, like people, don't want to
+            because they bring them comfort and happiness. Dragons are
+            protective of their hoards, because they, like people, wish not to
             lose what matters to them the most. I am much the same; driven to
             protect of what matters to me. Yet, things "taken" from my hoard,
             are not taken away from me. For me, every file "taken", every copy
-            made, simply assures me further, that what matters for me won't be
+            made, simply assures me further, that what's mattered to me won't be
             lost again.
         </p>
     </div>
@@ -94,15 +105,16 @@
 }
 
 .biography p {
-    display: block;
+    margin: 16px 0;
 }
 
 .biography p.wyrm {
+    text-align: right;
     opacity: 0.8;
 }
 
 .biography p.othr {
     font-style: italic;
-    opacity: 0.4;
+    color: #888A;
 }
 </style>
